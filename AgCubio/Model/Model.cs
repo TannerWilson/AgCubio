bfac62318c92627a9633f36cfba93e0316195f85
TannerWilson <tanner28aw@gmail.com>
2015-11-10 11:54:16 -0700

    Initial setup

---
+++ AgCubio/Model/Model.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Model
+    {
+        public class World
+        {
+            // Height and width of the world view screen
+            private readonly int Height;
+            private readonly int Width;
+
+            // Set of cubes in the world
+            private HashSet<Cube> cubes;
+
+            /// <summary>
+            /// Constructs a new world object given a height width and collection of cubes
+            /// </summary>
+            public World(int height, int width, HashSet<Cube> cubeset)
+            {
+                this.Height = height;
+                this.Width = width;
+                this.cubes = cubeset;
+            }
+        }
+
+        /// <summary>
+        ///  Helper class to "world" used to store all information associated with a given cube
+        /// </summary>
+        public class Cube
+        {
+            // Unique ID for cube
+            private string UID;
+            // Name of the cube
+            private string Name;
+            // Color of the cube
+            private ConsoleColor Color;
+            // Mass of the cube
+            private double Mass;
+            // If cube is food or not
+            private bool FoodStatus;
+            // Cubes X and Y positions in space
+            private int X;
+            private int Y;
+
+            /// <summary>
+            /// Constructor used to initialize each member variable
+            /// </summary>
+            public Cube(String id, string name, ConsoleColor color, double mass, bool status, int x, int y)
+            {
+                UID = id;
+                this.Name = name;
+                this.Color = color;
+                this.Mass = mass;
+                FoodStatus = status;
+                this.X = x;
+                this.Y = y;
+            }
+
+            /// <summary>
+            /// Returns the cube's width
+            /// </summary>
+            public double getWidth()
+            {
+                // Return square root of the mass
+                return Math.Sqrt(Mass);
+            }
+
+            /// <summary>
+            /// Returns the Y position of the top of the cube
+            /// </summary>
+            public int Top()
+            {
+                return Y - (int)this.getWidth();
+            }
+
+            /// <summary>
+            /// Returns the X position of the right edge of the cube
+            /// </summary>
+            public int Right()
+            {
+                return X + (int)this.getWidth();
+            }
+
+            /// <summary>
+            /// Returns the X position of the left edge of the cube
+            /// </summary>
+            public int Left()
+            {
+                return X - (int)this.getWidth();
+            }
+
+            /// <summary>
+            /// Returns the Y position of the bottom edge of the cube
+            /// </summary>
+            public int Bottom()
+            {
+                return Y + (int)this.getWidth();
+            }
+        }
+    }
+}
---
+++ AgCubio/Network_Controller/Network_Controller.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_Controller
+{
+    /// <summary>
+    /// Object used to represent the current state of the world.
+    /// Used to "know" what to do when information comes in form the server.
+    /// </summary>
+    public class PreservedState
+    {
+
+    }
+
+    public class Network
+    {
+        /// <summary>
+        /// This function attempts to connect to the server via a provided hostname. 
+        /// It saves the callback function (in a state object) for use when data arrives.
+        /// </summary>
+        /// <param name="callback"> A function inside the View to be called when a connection is made </param>
+        /// <param name="hostname"> The name of the server to connect to </param>
+        public Socket Connect_to_Server(Delegate callback, string hostname)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="state_in_an_ar_object"></param>
+        public void Connected_to_Server(IAsyncResult state_in_an_ar_object)
+        {
+
+        }
+
+        public void ReceiveCallback(IAsyncResult state_in_an_ar_object)
+        {
+
+        }
+
+        /// <summary>
+        /// Helper function that the client View code will call whenever it wants more data.
+        ///  Note: the client will probably want more data every time it gets data
+        /// </summary>
+        public void i_want_more_data(PreservedState state)
+        {
+
+        }
+
+        /// <summary>
+        /// This function will allow a program to send data over a socket. 
+        /// This function converts the data into bytes and then sends them using socket.BeginSend.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="data"></param>
+        public void Send(Socket socket, String data)
+        {
+
+        }
+
+        /// <summary>
+        /// This function "assists" the Send function. If all the data has been sent, 
+        /// then life is good and nothing needs to be done. If there is more data to send, 
+        /// the SendCallBack needs to arrange to send this data.
+        /// </summary>
+        public void SendCallBack()
+        {
+
+        }
+    }
+}
